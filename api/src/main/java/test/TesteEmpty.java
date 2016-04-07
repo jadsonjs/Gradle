3c9a3ab32fc8177869d69353fec4c251d694c4ee
Jadson Santos <jadson@info.ufrn.br>
2016-04-06 22:03:22 -0300

    gradle muiltiprojects
    Signed-off-by: Jadson Santos <jadson@info.ufrn.br>

---
+++ api/src/main/java/test/TesteEmpty.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) Automation Software Engineering Group
+ *
+ * This software is distributed WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND
+ */
+package test;
+
+/**
+ * @author jadson - jadsonjs@gmail.com
+ *
+ */
+public interface TesteEmpty {
+
+	public boolean verify(String s);
+}
